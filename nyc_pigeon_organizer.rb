685fe3301f49bd115e73fe0292ceb2148183becc
Keginold Philip <keginoldp@hotmail.com>
2019-08-23 12:12:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |key, inner_hash|
     inner_hash.values.flatten.each do |name|
-      #setting the names of the pigeons as keys with empty hashes
+       hashes
       if !new_hash[name]
         new_hash[name] = {}
       end
